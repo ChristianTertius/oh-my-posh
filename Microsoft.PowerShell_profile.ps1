771845e26faea29db32bc3401101974bca402b7f
ChristianTertius <ct160103@gmail.com>
2024-07-30 18:47:13 +0700

    first commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+#alias
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias dw 'Ctrl+w'
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
